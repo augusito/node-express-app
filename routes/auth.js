75bc6d6f3f48a49d5538f5fbc2698a62ab9703ec
augusito <kaugustus15@gmail.com>
2019-11-08 10:47:29 +0300

    Improve error handling

--- routes/auth.js
+++ routes/auth.js
@@ -1,31 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
 const jwt = require('jsonwebtoken');
 const passport = require("passport");
 
-router.post('/login', function (req, res, next) {
+router.post('/login', 
+    [
+    check('email').not().isEmpty().withMessage('Email field is required'),
+    check('password').not().isEmpty().withMessage('Password field is required')
+], function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({errors: errors.array()});
+    }
+    
     passport.authenticate('local', {session: false}, (err, user, info) => {
-        try {
-            if (err || !user) {
-                const error = new Error('An Error occurred');
-                return next(error);
-            }
-            req.login(user, {session: false}, (error) => {
-                if (error) {
-                    return next(error)
-                }
+        console.log(err);
+        console.log(user);
+        console.log(info);
 
-                //store user identity information: id, email, username
-                const body = {id: user.id, email: user.email, username: user.username};
-                //Sign the JWT token and populate the payload with the identity information
-                const token = jwt.sign({user: body}, 'secret');
-                //Send back the token to the user
-                return res.json({body, token});
-            });
-        } catch (error) {
-            return next(error);
+        if (err) {
+            res.status(401).json(err);
+        } else if (!user) {
+            res.status(401).json(info);
         }
-    })(req, res, next);
+
+        req.login(user, {session: false}, (err) => {
+            if (err) {
+                 res.status(401).json(err);
+            }
+
+            //store user identity information: id, email, username
+            const body = {id: user.id, email: user.email, username: user.username};
+            //Sign the JWT token and populate the payload with the identity information
+            const token = jwt.sign({user: body}, 'secret');
+            //Send back the token to the user
+            return res.json({body, token});
+        });
+    })(req, res);
 });
 
 module.exports = router;
